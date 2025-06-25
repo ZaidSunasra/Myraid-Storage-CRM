8426a8e9b3ab24ed2cd5be310b400cab6aac36d7
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-06-25 12:40:11 +0530

    Installed shadcn component and its wrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { UserProvider } from "./context/UserContext"
 import Router from "./routes/routes"
+import { Toaster } from "sonner"
 
 function App() {
   return (
     <>
       <UserProvider>
+        <Toaster richColors expand={true} closeButton/>
         <Router />
       </UserProvider>
     </>
