bc927e5210f7a3f9f565b26c341d5b59a1dc771e
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-06-25 12:20:06 +0530

    Added error messages to types and schema

--- shared/src/auth.schema.ts
+++ shared/src/auth.schema.ts
@@ -3,18 +3,19 @@ import { z } from "zod/v4";
 export const DEPARTMENTS = ["MARKETING", "ADMIN", "FACTORY", "DRAWING"] as const;
 
 export const loginSchema = z.object({
-    email: z.email(),
-    password: z.string().min(6)
+    email: z.email("Invalid email address"),
+    password: z.string().min(6, "Password should be atleast 6 characters long")
 });
 
 export const signupSchema = z.object({
-    first_name: z.string(),
-    last_name: z.string(),
-    email: z.email(),
-    phone: z.string(),
-    password: z.string().min(6),
+    first_name: z.string("First name required"),
+    last_name: z.string("Last name required"),
+    email: z.email("Invalid email address"),
+    phone: z.string().max(10, "Invalid phone number"),
+    password: z.string().min(6, "Password should be atleast 6 characters long"),
     department: z.enum(DEPARTMENTS),
     quotation_code: z.string().optional()
 });
 
 export type AddUser = z.infer<typeof signupSchema>;
+export type LoginUser = z.infer<typeof loginSchema>;
--- shared/src/leads.schema.ts
+++ shared/src/leads.schema.ts
@@ -1,19 +1,21 @@
-import { z } from "zod/v4"; 
+import { z } from "zod/v4";
 
 export const SOURCES = ["INDIAMART", "GOOGLEADS"] as const;
 
 export const leadSchema = z.object({
-    first_name: z.string(),
-    last_name: z.string(),
-    phone: z.string(),
-    email: z.string(),
+    first_name: z.string("First name required"),
+    last_name: z.string("Last name required"),
+    phone: z.string().max(10, "Invalid phone number"),
+    email: z.string("Invalid email address"),
     description: z.string().optional(),
     assigned_to: z.coerce.number(),
     source: z.enum(SOURCES),
-    product: z.string(),
-    company_name: z.string(),
-    address: z.string(),
+    product: z.string("Product required"),
+    company_name: z.string("Company name required"),
+    address: z.string("Address required"),
     gst_no: z.string().optional(),
 });
 
 export type AddLead = z.infer<typeof leadSchema>;
+
+export type EditLead = AddLead & { id: number };
