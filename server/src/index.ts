8e6fbf2e21d2522fe9ee62e3672a8bd55ca36957
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-06-25 12:20:45 +0530

    Configured CORS setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
 import  express from "express";
 import cookieparser from "cookie-parser";
+import cors from "cors";
 import { PORT } from "./utils/constant";
 import { mainRouter } from "./routes/route";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieparser());
 app.use("/api/v1", mainRouter);
