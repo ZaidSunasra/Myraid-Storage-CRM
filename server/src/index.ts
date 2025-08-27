b74f59e83047827c07a2741753797691a87a0db4
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-08-28 00:41:45 +0530

    Add url to .env variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import  express from "express";
 import cookieparser from "cookie-parser";
 import cors from "cors";
-import { PORT } from "./utils/constant";
+import { PORT, URL } from "./utils/constant";
 import { mainRouter } from "./routes/route";
 import registerCrons from "./cron-jobs/index.cron";
 
@@ -9,7 +9,7 @@ const app = express();
 
 registerCrons();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: URL,
     credentials: true
 }))
 app.use(express.json());
--- server/src/utils/constant.ts
+++ server/src/utils/constant.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+export const URL = process.env.FE_URL
 
 export const cookieOptions = {
     maxAge: 24 * 60 * 60 * 1000,
