9c1f28d674dc8e8d0f30f9b16b6ae4cfdd7d9b42
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-09-17 21:58:08 +0530

    types(quotation): add add quotation schema to shared package

--- shared/src/quotation.schema.ts
+++ shared/src/quotation.schema.ts
@@ -19,30 +19,39 @@ export const quotationItemSchema = z.object({
     market_rate: z.number(),
 })
 
-export type QuotationItem = z.infer<typeof quotationItemSchema>
-export const addQuotationSchema = z.object({
-    quotation_template: z.enum(QUOTATION_TEMPLATE),
-    product_type: z.enum(PRODUCT_TYPE),
-    bay: z.number().gte(1).lte(7),
-    compartment: z.number().gte(4).lte(7),
-    quotation_item: z.array(quotationItemSchema).optional(),
+export const quotationProductSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    items: z.array(quotationItemSchema),
     powder_coating: z.number(),
     trolley_material: z.number(),
-    sheet_material: z.number(),
+    ss_material: z.number(),
     total_weight: z.number(),
     labour_cost: z.number(),
     installation: z.number(),
     accomodation: z.number(),
     transport: z.number(),
     metal_rate: z.number(),
+    total_body: z.number(),
+    total_market_rate: z.number(),
+    total_provided_rate: z.number(),
+    set: z.number(),
+    profit_percent: z.number()
+})
+
+export const addQuotationSchema = z.object({
+    quotation_template: z.enum(QUOTATION_TEMPLATE),
+    product_type: z.enum(PRODUCT_TYPE),
+    bay: z.number().gte(1).lte(7),
+    compartment: z.number().gte(4).lte(7),
+    quotation_item: z.array(quotationProductSchema).optional(),
     total: z.number(),
     grandTotal: z.number(),
     gst: z.number(),
     discount: z.number(),
-    total_body: z.number(),
-    total_market_rate: z.number(),
-    total_provided_rate: z.number(),
     round_off: z.number(),
 })
 
+export type QuotationProduct = z.infer<typeof quotationProductSchema>;
+export type QuotationItem = z.infer<typeof quotationItemSchema>
 export type AddQuotation = z.infer<typeof addQuotationSchema>;
