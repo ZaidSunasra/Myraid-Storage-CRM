4d4b8dffc1af6a923b8746b7b2fbe5913db9bca6
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-09-23 23:52:54 +0530

    feat(quotation): add preview quotation ui to view quotation details at a glance

--- client/src/features/quotation/pages/AddQuotationPage.tsx
+++ client/src/features/quotation/pages/AddQuotationPage.tsx
@@ -1,88 +1,118 @@
-import Navbar from "@/shared/components/Navbar";
-import { Button } from "@/shared/components/ui/button";
-import { ArrowLeft } from "lucide-react";
-import { NavLink, useParams } from "react-router";
-import QuotationSideBar from "../component/QuotationSideBar";
-import { useState } from "react";
-import { Form } from "@/shared/components/ui/form";
-import QuotationProductSelector from "../component/QuotationProductSelector";
-import QuotationCosting from "../component/QuotationCosting";
-import QuotationSummary from "../component/QuotationSummary";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useQuotation } from "@/context/QuotationContext";
-import { useAddQuotation } from "@/api/quotations/quotations.mutation";
-import { addQuotationSchema, type AddQuotation } from "zs-crm-common";
+import Navbar from "@/shared/components/Navbar"
+import { Button } from "@/shared/components/ui/button"
+import { ArrowLeft, Eye, X } from "lucide-react"
+import { NavLink, useParams } from "react-router"
+import QuotationSideBar from "../component/QuotationSideBar"
+import { useState } from "react"
+import { Form } from "@/shared/components/ui/form"
+import QuotationProductSelector from "../component/QuotationProductSelector"
+import QuotationCosting from "../component/QuotationCosting"
+import QuotationSummary from "../component/QuotationSummary"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useQuotation } from "@/context/QuotationContext"
+import { useAddQuotation } from "@/api/quotations/quotations.mutation"
+import { addQuotationSchema, type AddQuotation } from "zs-crm-common"
+import PreviewQuotationPage from "./PreviewQuotationPage"
 
 
 const AddQuotationPage = () => {
+  const { id } = useParams()
+  const [currentStep, setCurrentStep] = useState<number>(1)
+  const [showPreview, setShowPreview] = useState<boolean>(false) 
+  const { products } = useQuotation()
+  const addQuotation = useAddQuotation()
 
-    const { id } = useParams();
-    const [currentStep, setCurrentStep] = useState<number>(1);
-    const { products } = useQuotation();
-    const addQuotation = useAddQuotation();
+  const form = useForm<AddQuotation>({
+    resolver: zodResolver(addQuotationSchema),
+    defaultValues: {
+      quotation_template: undefined,
+      product_type: undefined,
+      bay: 1,
+      compartment: 4,
+      quotation_item: products,
+      gst: 18,
+      grandTotal: 0,
+      discount: 0,
+      round_off: 0,
+    },
+  })
 
-    const form = useForm<AddQuotation>({
-        resolver: zodResolver(addQuotationSchema),
-        defaultValues: ({
-            quotation_template: undefined,
-            product_type: undefined,
-            bay: 1,
-            compartment: 4,
-            quotation_item: products,
-            gst: 18,
-            grandTotal: 0,
-            discount: 0,
-            round_off: 0,
-        }),
-    });
+  const handleNext = () => setCurrentStep((step) => step + 1)
+  const handlePrev = () => setCurrentStep((step) => step - 1)
 
-    const handleNext = () => {
-        setCurrentStep((step) => step + 1)
-    }
+  const onSubmit = (data: AddQuotation) => {
+    const payload = { ...data, quotation_item: products }
+    console.log(payload)
+    addQuotation.mutate({ data: payload, deal_id: id as string })
+  }
 
-    const handlePrev = () => {
-        setCurrentStep((step) => step - 1)
-    }
-
-    const onSubmit = (data: AddQuotation) => {
-        const payload = {
-            ...data,
-            quotation_item: products,
-        }
-        console.log(payload)
-       addQuotation.mutate({data: payload, deal_id:id as string})
-    }
-
-    return <div className="bg-accent min-h-screen">
-        <Navbar />
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <div className="px-4 py-6 sm:px-0">
-                <div className="flex items-center space-x-4 mb-6">
-                    <NavLink to={`/deal/${id}`}>
-                        <Button variant="ghost" size="icon">
-                            <ArrowLeft className="h-4 w-4" />
-                        </Button>
-                    </NavLink>
-                    <div>
-                        <h1 className="text-2xl font-bold text-foreground">Add New Quotation</h1>
-                        <p className="text-muted-foreground">Create a new quotation in your CRM system</p>
-                    </div>
-                </div>
-                <div className="flex flex-col  gap-8">
-                    <QuotationSideBar currentStep={currentStep} />
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            {currentStep === 1 && <QuotationProductSelector form={form} handleNext={handleNext} />}
-                            {currentStep === 2 && <QuotationCosting form={form} handleNext={handleNext} handlePrev={handlePrev} />}
-                            {currentStep === 3 && <QuotationSummary form={form} handlePrev={handlePrev} />}
-                        </form>
-                    </Form>
-                </div>
+  return (
+    <div className="bg-accent min-h-screen">
+      <Navbar />
+      <div className="mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <div className="flex justify-between flex-col sm:flex-row">
+            <div className="flex items-center space-x-4 mb-6">
+              <NavLink to={`/deal/${id}`}>
+                <Button variant="ghost" size="icon">
+                  <ArrowLeft className="h-4 w-4" />
+                </Button>
+              </NavLink>
+              <div>
+                <h1 className="text-2xl font-bold text-foreground">Add New Quotation</h1>
+                <p className="text-muted-foreground">
+                  Create a new quotation in your CRM system
+                </p>
+              </div>
             </div>
+            {!showPreview ? (
+              <Button className="mb-4 flex gap-2" onClick={() => setShowPreview(true)}>
+                <Eye className="w-4 h-4" />
+                Preview Quotation
+              </Button>
+            ) : (
+              <Button
+                variant="destructive"
+                className="mb-4 flex gap-2"
+                onClick={() => setShowPreview(false)}
+              >
+                <X className="w-4 h-4" />
+                Close Preview
+              </Button>
+            )}
+          </div>
+          {!showPreview ? (
+            <div className="flex flex-col gap-8">
+              <QuotationSideBar currentStep={currentStep} />
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  {currentStep === 1 && (
+                    <QuotationProductSelector form={form} handleNext={handleNext} />
+                  )}
+                  {currentStep === 2 && (
+                    <QuotationCosting
+                      form={form}
+                      handleNext={handleNext}
+                      handlePrev={handlePrev}
+                    />
+                  )}
+                  {currentStep === 3 && (
+                    <QuotationSummary form={form} handlePrev={handlePrev} />
+                  )}
+                </form>
+              </Form>
+            </div>
+          ) : (
+            <div className="bg-white shadow rounded-xl p-4 overflow-auto">
+              <h2 className="text-lg font-semibold mb-4">Quotation Preview</h2>
+              <PreviewQuotationPage data={form.watch()} />
+            </div>
+          )}
         </div>
-    </div >
-};
-
+      </div>
+    </div>
+  )
+}
 
 export default AddQuotationPage
---
+++ client/src/features/quotation/pages/PreviewQuotationPage.tsx
@@ -0,0 +1,295 @@
+import { useQuotation } from "@/context/QuotationContext"
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow, } from "@/shared/components/ui/table"
+import type { QuotationItem, QuotationProduct } from "zs-crm-common";
+
+const PreviewQuotationPage = ({ data }: { data: any }) => {
+
+  const { products, overallTotal } = useQuotation();
+  console.log(data)
+  console.log(products)
+
+  const calculateProductTotal = (product: QuotationProduct, item?: QuotationItem) => {
+    const productTotal = Number(product.total_provided_rate) + Number(product.installation) * Number(product.total_body) + Number(product.transport) + Number(product.accomodation);
+    const profitTotal = Number(Number(productTotal) * (1 + (Number(product.profit_percent) / 100))).toFixed(2);
+    const doorItem = product.items.find((item: any) => item.name === "DOOR");
+    const doorTotal = doorItem ? doorItem.provided_rate : 0;
+    const qtyRatio = Number(item?.per_bay_qty) / Number(product.total_body);
+    const itemWiseTotal = ((Number(profitTotal) - doorTotal) * qtyRatio).toFixed(2);
+    return { productTotal, profitTotal, doorTotal, itemWiseTotal };
+  }
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="space-y-6">
+        <Table className="border border-black">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="border border-black w-12 text-center">
+                Sr. No
+              </TableHead>
+              <TableHead className="border border-black">
+                Description of Goods
+              </TableHead>
+              <TableHead className="border border-black text-center">
+                Qty
+              </TableHead>
+              <TableHead className="border border-black text-center">
+                Rate
+              </TableHead>
+              <TableHead className="border border-black text-center">
+                Amount
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          {data.quotation_template === "item_wise" ?
+            <>
+              {products.map((product) => {
+                return (
+                  <TableBody>
+                    {product.items.map((item: any, index: number) => {
+                      const compartment = product.name[6];
+                      const { itemWiseTotal } = calculateProductTotal(product, item)
+                      return (
+                        <TableRow key={item.id} className="align-top">
+                          <TableCell className="border border-black text-center">
+                            {index + 1}
+                          </TableCell>
+                          <TableCell className="border border-black">
+                            <div className="font-semibold">
+                              {item.name}{" "}
+                              {item.code ? `(${item.code})` : ""}{" "}
+                            </div>
+                            <div className="text-xs text-muted-foreground">
+                              {item.name !== "DOOR" ? `${item.default_height} (HT) x ${item.default_width} (W) x ${item.default_depth} (D) MM` : ""}{" "}
+                              {item.name !== "DOOR" ? compartment ? `${compartment} Compartments` : "" : ""}
+                            </div>
+                          </TableCell>
+                          <TableCell className="border border-black text-center">
+                            {item.qty}
+                          </TableCell >
+                          <TableCell className="border border-black text-center">
+                            {item.name === "DOOR" ? `${item.provided_rate}` : `${itemWiseTotal}`}
+                          </TableCell>
+                          <TableCell className="border border-black text-center">
+                            {item.name === "DOOR" ? `${Number(item.provided_rate) * Number(item.qty)}` : `${Number(Number(item.qty) * Number(itemWiseTotal)).toFixed(2)}`}
+                          </TableCell>
+                        </TableRow>
+                      )
+                    })}
+                  </TableBody>
+                )
+              })}
+            </>
+            : <>
+              {products.map((product) => {
+                return (
+                  <TableBody>
+                    {product.items.map((item: any, index: number) => {
+                      const compartment = product.name[6];
+                      const { profitTotal } = calculateProductTotal(product, item)
+                      return (
+                        <TableRow key={item.id} className="align-top">
+                          <TableCell className="border border-black text-center">
+                            {index + 1}
+                          </TableCell>
+                          <TableCell className="border border-black">
+                            <div className="font-semibold">
+                              {item.name}{" "}
+                              {item.code ? `(${item.code})` : ""}{" "}
+                            </div>
+                            <div className="text-xs text-muted-foreground">
+                              {item.name !== "DOOR" ? `${item.default_height} (HT) x ${item.default_width} (W) x ${item.default_depth} (D) MM` : ""}{" "}
+                              {item.name !== "DOOR" ? compartment ? `${compartment} Compartments` : "" : ""}
+                            </div>
+                          </TableCell>
+                          {index === 0 &&
+                            <>
+                              <TableCell rowSpan={product.items.length} className="border border-black text-center">
+                                {product.set} SET
+                              </TableCell>
+                              <TableCell rowSpan={product.items.length} className="border border-black text-center">
+                                {profitTotal}
+                              </TableCell>
+                              <TableCell rowSpan={product.items.length} className="border border-black text-center">
+                                {Number(profitTotal) * Number(product.set)}
+                              </TableCell>
+                            </>
+                          }
+                        </TableRow>
+                      )
+                    })}
+                  </TableBody>
+                )
+              })}
+            </>
+          }
+          <TableFooter>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell className="border-r border-black"></TableCell>
+              <TableCell colSpan={2} className="border-r border-black">Total</TableCell>
+              <TableCell>{overallTotal.toFixed(2)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell className="border-r border-black"></TableCell>
+              <TableCell colSpan={2} className="border-r border-black">GST {data.gst}%</TableCell>
+              <TableCell>{(Number(overallTotal) *  Number(data.gst) / 100).toFixed(2)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell className="border-r border-black"></TableCell>
+              <TableCell colSpan={2} className="border-r border-black">Round Off</TableCell>
+              <TableCell>{data.round_off}</TableCell>
+            </TableRow>
+             <TableRow>
+              <TableCell></TableCell>
+              <TableCell className="border-r border-black"></TableCell>
+              <TableCell colSpan={2} className="border-2 border-black border-r-0">Grand Total</TableCell>
+              <TableCell className="border-2 border-black border-l-0">{data.grandTotal}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+        {products.map((product) => (
+          <Table className="border border-black" key={product.id}>
+            <TableHeader>
+              <TableRow>
+                <TableHead rowSpan={2} className="border border-black">Sr. No</TableHead>
+                <TableHead colSpan={3} className="border border-black">Description of Goods</TableHead>
+                <TableHead rowSpan={2} className="border border-black">Installation Body</TableHead>
+              </TableRow>
+              <TableRow>
+                <TableHead className="border border-black">Name</TableHead>
+                <TableHead className="border border-black">Per bay qty</TableHead>
+                <TableHead className="border border-black"> Qty</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {
+                product.items.map((item: any, index: number) => (
+                  <>
+                    {
+                      item.name !== "DOOR" &&
+                      <TableRow key={item.id}>
+                        <TableCell className="border border-black">{index + 1} </TableCell>
+                        <TableCell className="border border-black">{item.name}</TableCell>
+                        <TableCell className="border border-black">{item.per_bay_qty}</TableCell>
+                        <TableCell className="border border-black">{item.qty}</TableCell>
+                        <TableCell className="border border-black">{Number(item.per_bay_qty) * Number(item.qty)}</TableCell>
+                      </TableRow>
+                    }
+                  </>
+                ))
+              }
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={4}>Total Body</TableCell>
+                <TableCell>{product.total_body}</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        ))}
+      </div>
+      <div className="space-y-6">
+        {products.map((product) => {
+          const { productTotal, profitTotal } = calculateProductTotal(product)
+          return (
+            <>
+              <Table className="border border-black">
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="border border-black">Labour</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black">{product.labour_cost}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Material</TableCell>
+                    <TableCell className="border border-black">{product.ss_material}</TableCell>
+                    <TableCell className="border border-black">{product.trolley_material}</TableCell>
+                    <TableCell className="border border-black">{Number(product.ss_material) + Number(product.trolley_material)}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Total Weight</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black">{product.total_weight}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Powder Coating</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black">{product.powder_coating}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              <Table className="border border-black">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="border border-black">Qty</TableHead>
+                    <TableHead className="border border-black">Provided Rate</TableHead>
+                    <TableHead className="border border-r-2 border-black">Total</TableHead>
+                    <TableHead className="border border-black">Market Rate</TableHead>
+                    <TableHead className="border border-black">Total</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {product.items.map((item: any) => (
+                    <TableRow key={item.id}>
+                      <TableCell className="border border-black">{item.qty}</TableCell>
+                      <TableCell className="border border-black">{item.provided_rate}</TableCell>
+                      <TableCell className="border border-r-2 border-black">{Number(item.qty) * Number(item.provided_rate)}</TableCell>
+                      <TableCell className="border border-black">{item.market_rate}</TableCell>
+                      <TableCell className="border border-black">{Number(item.qty) * Number(item.market_rate)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Table className="border border-black">
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="border border-black">Installation</TableCell>
+                    <TableCell className="border border-black">{product.installation}</TableCell>
+                    <TableCell className="border border-black">{Number(product.installation) * Number(product.total_body)}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Accomodation</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black">{product.accomodation}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Transport</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black">{product.transport}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              <Table className="border border-black">
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="border border-black">Total</TableCell>
+                    <TableCell className="border border-black"></TableCell>
+                    <TableCell className="border border-black"> {productTotal} </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Profit (%)</TableCell>
+                    <TableCell className="border border-black">{product.profit_percent}</TableCell>
+                    <TableCell className="border border-black">{Number(profitTotal)}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell className="border border-black">Set</TableCell>
+                    <TableCell className="border border-black">{product.set}</TableCell>
+                    <TableCell className="border border-black">{Number(profitTotal) * Number(product.set)}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default PreviewQuotationPage
