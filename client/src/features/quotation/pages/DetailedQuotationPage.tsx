c5c5bc6c11780ca42e8d8fa0536b648134069050
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-10-01 19:32:02 +0530

    feat(quotation): implement copy quotation functionality

--- client/src/features/quotation/pages/DetailedQuotationPage.tsx
+++ client/src/features/quotation/pages/DetailedQuotationPage.tsx
@@ -7,15 +7,19 @@ import { Button } from "@/shared/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/components/ui/tabs";
 import { toTitleCase } from "@/utils/formatData";
 import { canView } from "@/utils/viewPermission";
-import { ArrowLeft, Edit, FileText } from "lucide-react";
+import { ArrowLeft, Copy, Edit, FileText } from "lucide-react";
 import { NavLink, useNavigate, useParams } from "react-router"
 import QuotationDetails from "../component/QuotationDetails";
 import QuotationWorkingDetails from "../component/QuotationWorkingDetails";
 import type { GetQuotationOutput } from "zs-crm-common";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/shared/components/ui/dialog";
+import { useState } from "react";
+import CopyQuotationForm from "../component/CopyQuotationForm";
 
 const DetailedQuotationPage = () => {
 
     const { id, quotation_id } = useParams();
+    const [open, setOpen] = useState<boolean>(false);
     const { data, isError, isPending } = FetchQuotationById(quotation_id as string);
     const { user } = useUser();
     const navigate = useNavigate();
@@ -36,9 +40,9 @@ const DetailedQuotationPage = () => {
                         </NavLink>
                         <div>
                             <h1 className="text-2xl font-bold ">
-                                {data.quotation && data.quotation.quotation_products.length > 1 ? 
-                                toTitleCase(data.quotation.quotation_products[0].name) + ` & ${data.quotation.quotation_products.length - 1} Other Product`
-                                : toTitleCase(data.quotation?.quotation_products[0].name as string)}
+                                {data.quotation && data.quotation.quotation_products.length > 1 ?
+                                    toTitleCase(data.quotation.quotation_products[0].name) + ` & ${data.quotation.quotation_products.length - 1} Other Product`
+                                    : toTitleCase(data.quotation?.quotation_products[0].name as string)}
                             </h1>
                             <p className="text-gray-600">{toTitleCase(data.quotation?.deal.company.name as string)}</p>
                         </div>
@@ -75,15 +79,32 @@ const DetailedQuotationPage = () => {
                 <div className="lg:col-span-1">
                     <div className="w-full">
                         <Button className="mb-8 text-white flex gap-2 px-6 py-2 rounded-xl shadow-md transition w-full bg-blue-600 hover:bg-blue-700"
-                         onClick={() => window.open(`/quotation/print/${id}/${quotation_id}`, "_blank")}
-                         >
+                            onClick={() => window.open(`/quotation/print/${id}/${quotation_id}`, "_blank")}
+                        >
                             <FileText className="w-4 h-4" />
                             View Quotation
+                        </Button>
+                    </div>
+                    <div className="w-full">
+                        <Button className="mb-8 text-white flex gap-2 px-6 py-2 rounded-xl shadow-md transition w-full" onClick={() => setOpen(true)}>
+                            <Copy className="w-4 h-4" />
+                            Copy Quotation
                         </Button>
                     </div>
                 </div>
             </div>
         </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>Copy Quotation</DialogTitle>
+                    <DialogDescription>
+                        Enter the Deal ID where you want to copy this quotation.
+                    </DialogDescription>
+                    <CopyQuotationForm />
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     </div>
 
 }
